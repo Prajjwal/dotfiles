ab8b101fb2c7db9bc6cf60a64a2045d294649c06
Prajjwal Singh <prajjwal.singh@gmail.com>
2013-06-22 01:14:36 +0530

    Remove devilspie/. Add new devilspie2 config.

---
+++ devilspie2/config.lua
@@ -0,0 +1,37 @@
+-- Print the window and application name of everything opened if running with
+-- the --debug option. Useful.
+
+debug_print("Window Name: " .. get_window_name());
+debug_print("App Name: " .. get_application_name());
+
+-- Firefox
+if (get_application_name() == "Firefox") then
+
+	set_window_workspace(1);
+	maximize();
+
+end
+
+-- Pidgin
+if (get_application_name() == "Pidgin") then
+
+	set_window_workspace(2);
+
+	if (get_window_name() == "Buddy List") then
+		set_window_geometry(500, 100, 260, 750);
+	else
+		-- Wait half a second before attempting to maximize. Weird behaviour
+		-- otherwise. Couldn't find a solution on the interwebs.
+		os.execute("sleep 0.5");
+		maximize();
+	end
+
+end
+
+-- Transmission
+if (get_application_name() == "Transmission") then
+	
+	set_window_workspace(3);
+	minimize();
+
+end
