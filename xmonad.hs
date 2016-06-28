96d7b2bde5944d9c69d895cb30e8305898b837e9
Prajjwal Singh <sin@prajjwal.com>
2016-06-28 10:53:56 +0530

    Update Xmonad <-> Xmobar config

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,24 @@
+-- Imports
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 
-main = xmonad =<< xmobar myConfig
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being
+-- written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig
     { terminal = "urxvt"
     , borderWidth = 3
     }
+
