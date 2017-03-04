6ba0988ecde2da910da4ac959f93a2b38e7aba41
Prajjwal Singh <sin@prajjwal.com>
2017-03-04 11:36:23 +0530

    Better dependency checking for scripts

---
+++ bin/include/check_dependencies.sh
@@ -0,0 +1,12 @@
+function ensure_presence_of {
+	for dependency in $@; do
+		if ! hash $dependency 2> /dev/null; then
+			echo "$dependency is not installed"
+			exit 1
+		fi
+	done
+}
+
+function is_installed {
+	hash $1 2> /dev/null
+}
